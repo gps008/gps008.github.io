a1c90a1179c938732e7ea2092b604df955515870
Ramy <ramy.herrira@gmail.com>
2018-04-13 17:16:06 +0100

    add sender id

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -14,7 +14,7 @@ importScripts('https://www.gstatic.com/firebasejs/4.8.1/firebase-messaging.js');
 // Initialize the Firebase app in the service worker by passing in the
 // messagingSenderId.
 firebase.initializeApp({
-  'messagingSenderId': 'YOUR-SENDER-ID'
+  'messagingSenderId': '687681730428'
 });
 
 // Retrieve an instance of Firebase Messaging so that it can handle background
